beacbc67545636e7fbc0d040b4a0eeb43370d8ae
Thijs Lander <45820600+t-lander@users.noreply.github.com>
2026-02-11 21:44:17 +0100

    Add dynamic routes to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,15 @@
+import { getAllArticles } from "@/lib/contentful";
 import type { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const articles = await getAllArticles();
+    const articleEntries: MetadataRoute.Sitemap = articles.map((article) => ({
+        url: `https://thijslander.com/articles/${article.slug}`,
+        lastModified: new Date(article.sys.publishedAt),
+        changeFrequency: "monthly",
+        priority: 1,
+    }));
+
     return [
         {
             url: "https://thijslander.com",
@@ -20,5 +29,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: "weekly",
             priority: 0.8,
         },
+        ...articleEntries,
     ];
 }
